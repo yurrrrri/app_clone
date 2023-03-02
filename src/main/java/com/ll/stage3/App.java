e243f1574f73ac87eb4f6643eb681a7bddb19417
yurrrrri <yu_ri_son@naver.com>
2023-03-02 18:12:00 +0900

    stage3

---
+++ src/main/java/com/ll/stage3/App.java
@@ -0,0 +1,30 @@
+package com.ll.stage3;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        while(true){
+            System.out.print("명령) ");
+            String command = sc.nextLine().trim();
+
+            if(command.equals("종료")){
+                break;
+            } else if(command.equals("등록")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+        }
+    }
+}
